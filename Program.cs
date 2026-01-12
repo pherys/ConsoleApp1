04713ce9ca528b66f24d4529cfda341726b29ee3
Fernanda Brito <93785872+pherys@users.noreply.github.com>
2026-01-12 20:10:30 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("=== TABUADA COMPLETA (1 AO 10) ===\n");
+
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.WriteLine($"--- Tabuada do {i} ---");
+
+            for (int j = 1; j <= 10; j++)
+            {
+                Console.WriteLine($"{i} x {j} = {i * j}");
+            }
+
+            Console.WriteLine();
+        }
+
+        Console.ReadLine();
+    }
+}
